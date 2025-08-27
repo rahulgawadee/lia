0e8f2619b307c22903eccdee1f9b06d4ad2f0fa7
Rahul Gawade <rahulgawade360@gmail.com>
2025-08-27 14:06:17 +0530

    Updated some issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,10 @@ import InternshipTypes from './components/InternshipTypes';
 import Students from './components/Students';
 import Footer from './components/Footer';
 
-import WhatIsLia from './Pages/WhatIsLia';
+import WhatIsLia from './Pages/WhatIsLia.jsx';
 import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 
-import DemoForm from './Pages/DemoForm';
+import DemoForm from './Pages/DemoForm.jsx';
 
 function HomePage() {
   return (
